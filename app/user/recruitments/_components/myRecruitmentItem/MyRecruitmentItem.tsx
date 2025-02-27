0c7727a39655ed21eb5098d65f3ca41d1fb4ee5e
황혜인 <119389723+hwanghaein@users.noreply.github.com>
2025-02-27 11:01:30 +0900

    [feat] 마이페이지 모집글 조회 스타일 수정

--- app/user/recruitments/_components/myRecruitmentItem/MyRecruitmentItem.tsx
+++ app/user/recruitments/_components/myRecruitmentItem/MyRecruitmentItem.tsx
@@ -4,32 +4,37 @@ import styles from "./MyRecruitmentItem.module.scss";
 
 import { FaHeart, FaEye, FaComment } from "react-icons/fa";
 
-export default function CreateItem() {
+export default function MyRecruitmentItem() {
   return (
     <div>
-      <div className={styles["mycreateitem__post-item"]}>
-        <div className={styles["mycreateitem__post-content"]}>
-          <div className={styles["mycreateitem__post-header"]}>
+      <div className={styles["myrecruitmentitem__post-item"]}>
+        <div className={styles["myrecruitmentitem__post-content"]}>
+          <div className={styles["myrecruitmentitem__post-header"]}>
             <Badge color="primary" fontColor="white" width={60} height={24} borderRadius={16} fontSize={12}>
               모집중
             </Badge>
-            <h2 className={styles["mycreateitem__post-title"]}>프로젝트 제목</h2>
+            <h2 className={styles["myrecruitmentitem__post-title"]}>프로젝트 제목</h2>
           </div>
-          <p className={styles["mycreateitem__post-description"]}>프로젝트 설명이 여기에 들어갑니다.</p>
-          <div className={styles["mycreateitem__post-meta"]}>
+          <p className={styles["myrecruitmentitem__post-description"]}>
+            프로젝트 설명이 여기에 들어갑니다.프로젝트 설명이 여기에 들어갑니다.프로젝트 설명이 여기에
+            들어갑니다.프로젝트 설명이 여기에 들어갑니다.프로젝트 설명이 여기에 들어갑니다.프로젝트 설명이 여기에
+            들어갑니다.프로젝트 설명이 여기에 들어갑니다.프로젝트 설명이 여기에 들어갑니다.프로젝트 설명이 여기에
+            들어갑니다.
+          </p>
+          <div className={styles["myrecruitmentitem__post-meta"]}>
             <div>
-              <span className={styles["mycreateitem__post-author"]}>신짱구</span>
-              <span className={styles["mycreateitem__post-dot"]}>·</span>
-              <span className={styles["mycreateitem__post-date"]}>5분 전</span>
+              <span className={styles["myrecruitmentitem__post-author"]}>신짱구</span>
+              <span className={styles["myrecruitmentitem__post-dot"]}>·</span>
+              <span className={styles["myrecruitmentitem__post-date"]}>5분 전</span>
             </div>
-            <div className={styles["mycreateitem__post-stats"]}>
-              <div className={styles["mycreateitem__post-stats-heart"]}>
+            <div className={styles["myrecruitmentitem__post-stats"]}>
+              <div className={styles["myrecruitmentitem__post-stats-heart"]}>
                 <FaHeart /> <span>12</span>
               </div>
-              <div className={styles["mycreateitem__post-stats-eye"]}>
+              <div className={styles["myrecruitmentitem__post-stats-eye"]}>
                 <FaEye /> <span>34</span>
               </div>
-              <div className={styles["mycreateitem__post-stats-comment"]}>
+              <div className={styles["myrecruitmentitem__post-stats-comment"]}>
                 <FaComment /> <span>5</span>
               </div>
             </div>
