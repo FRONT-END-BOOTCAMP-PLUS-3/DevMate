8c88255849e8e2aaac2982d97e595df1afc08089
황혜인 <119389723+hwanghaein@users.noreply.github.com>
2025-02-28 21:42:18 +0900

    [feat] 글쓰기 버튼 누를 시 글쓰기 페이지로 라우팅

--- app/(anon)/recruitments/_components/recruitmentsWriteButton/recruitmentsWriteButton.tsx
+++ app/(anon)/recruitments/_components/recruitmentsWriteButton/recruitmentsWriteButton.tsx
@@ -1,11 +1,13 @@
-"use client";
+import Link from "next/link";
 
 import styles from "./recruitmentsWriteButton.module.scss";
 
 export default function RecruitmentsWriteButton() {
   return (
     <div>
-      <button className={styles["main__write-button"]}>글쓰기</button>
+      <Link href="/user/recruitments/create" className={styles["main__write-button"]}>
+        글쓰기
+      </Link>
     </div>
   );
 }
