ef862935db21591dec40b0fcdb2d9b72198265ab
yezzero <yeyeonggim06@gmail.com>
2025-02-28 13:53:11 +0900

    fix: getHours 함수 제거

--- utils/formatDateToString.ts
+++ utils/formatDateToString.ts
@@ -1,5 +1,4 @@
 export const formatDateToString = (date: Date): string => {
   const newDate = new Date(date);
-  newDate.setHours(date.getHours() + 9);
   return `${newDate.getFullYear()}년 ${newDate.getMonth() + 1}월 ${newDate.getDate()}일`;
 };
