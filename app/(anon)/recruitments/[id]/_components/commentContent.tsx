38a6e11788e13295adf66793fafcd468ca838d0c
smimdla0205 <sangmin0205@gmail.com>
2025-03-01 18:35:10 +0900

    feat: 빌드에러 개선 중중

--- app/(anon)/recruitments/[id]/_components/commentContent.tsx
+++ app/(anon)/recruitments/[id]/_components/commentContent.tsx
@@ -6,7 +6,7 @@ import { useMemo, useState } from "react";
 
 import styles from "./commentContent.module.scss";
 
-import type { CommentDetailDto } from "@/application/usecases/projectDetail/dtos/recruitmentDetailDto";
+import type { CommentDetailDto } from "@/application/usecases/recruitment/dtos/recruitmentDetailDto";
 
 import CommentForm from "./commentForm";
 
