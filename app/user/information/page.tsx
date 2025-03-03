aca206de4c442b4f0ef2b04931b529716d7b3755
smimdla0205 <sangmin0205@gmail.com>
2025-03-03 19:09:16 +0900

    feat: 사용자 정보 페이지에 기본 및 상세 정보 상태 추가 및 입력 처리 개선

--- app/user/information/page.tsx
+++ app/user/information/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 
+import type { ChangeEvent } from "react";
 import { useEffect, useState } from "react";
 
 import Button from "@/components/button/button";
@@ -16,6 +17,11 @@ import { CAREER_EXP_OPTIONS, POSITION_OPTIONS, TECH_STACK_OPTIONS } from "@/cons
 
 import type { SelectOption } from "@/types";
 import type { MultiValue, SingleValue } from "react-select";
+import type {
+  InfoUserDto,
+  UserEditInfoDto,
+  UserNonEditInfoDto,
+} from "@/application/usecases/information/dtos/infoUserDto";
 
 import InfoUserRow from "./_components/infoUserRow";
 import { useEditUserInfo } from "./_hooks/use-editUserInfo";
@@ -36,11 +42,10 @@ export default function Information() {
     container__img__title,
   } = styles;
   const { state, dispatch } = useEditUserInfo();
+  const [userBasicInfo, setUserBasicInfo] = useState<UserNonEditInfoDto>();
+  const [userDetailInfo, setUserDetailInfo] = useState<UserEditInfoDto>();
   const [edit, setEdit] = useState<boolean>(false);
   const [isAddrSearchOpen, setIsAddrSearchOpen] = useState(false);
-  const [userId, setUserId] = useState<string>("");
-  const [userInfo, setUserInfo] = useState<any>(null);
-  const editClickHandler = () => setEdit(!edit);
 
   const selectChangeHandler = (
     selected: SingleValue<SelectOption> | MultiValue<SelectOption> | null,
@@ -54,13 +59,22 @@ export default function Information() {
       dispatch({ type: "SET_CAREER", value: selected as SingleValue<SelectOption> | null });
     }
   };
-  console.log(userId, "//", userInfo);
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: "SET_INPUT",
+      name: e.target.name as keyof typeof state,
+      value: e.target.value,
+    });
+  };
+  const editClickHandler = () => {
+    setEdit(!edit);
+    console.log(state);
+  };
+
   useEffect(() => {
     const getTokenAndUserInfo = async () => {
       try {
         const id = await decodeToken("id");
-
-        setUserId(id as string); // 디코딩된 ID로 상태 업데이트
         const response = await fetch("/api/information", {
           method: "POST",
           headers: {
@@ -71,15 +85,15 @@ export default function Information() {
         if (!response.ok) {
           throw new Error("Failed to fetch user information.");
         }
-        const data = await response.json();
-        setUserInfo(data); // 사용자 정보 상태 업데이트
+        const data: InfoUserDto = await response.json();
+        setUserBasicInfo(data.nonEditInfo);
+        setUserDetailInfo(data.editInfo);
       } catch (error) {
-        setUserInfo(null); // 오류 발생 시 사용자 정보 초기화
+        console.error(error);
       }
     };
-
     getTokenAndUserInfo();
-  }, []); // 컴포넌트가 처음 렌더링될 때만 실행
+  }, []);
   // axios로 변경
   return (
     <div className={container}>
@@ -91,12 +105,18 @@ export default function Information() {
         <div className={container__section__content}>
           <div className={container__img}>
             <span className={container__img__title}>이미지</span>
-            <Image src="/defaultProfile.svg" alt="유저 프로필" width={100} height={100} />
+            <Image src={userDetailInfo?.profileImg ?? "/test.jpg"} alt="유저 프로필" width={100} height={100} />
           </div>
-          <InfoUserRow title="닉네임" edit={edit}>
-            <InputField name="nickname" type="text" placeholder="닉네임" value={state.nickname} />
+          <InfoUserRow title="닉네임" edit={edit} info={userDetailInfo?.nickname}>
+            <InputField
+              name="nickname"
+              type="text"
+              placeholder="닉네임"
+              value={state.nickname}
+              onChange={changeHandler}
+            />
           </InfoUserRow>
-          <InfoUserRow title="경력유무" edit={edit}>
+          <InfoUserRow title="경력유무" edit={edit} info={`${String(userDetailInfo?.career)} 년`}>
             <Selector
               name="career"
               placeholder="n 년"
@@ -106,7 +126,7 @@ export default function Information() {
               onChange={(selected) => selectChangeHandler(selected, "career")}
             />
           </InfoUserRow>
-          <InfoUserRow title="직무" edit={edit}>
+          <InfoUserRow title="직무" edit={edit} info={userDetailInfo?.position}>
             <Selector
               name="position"
               placeholder="직무"
@@ -116,7 +136,7 @@ export default function Information() {
               onChange={(selected) => selectChangeHandler(selected, "position")}
             />
           </InfoUserRow>
-          <InfoUserRow title="기술스택" edit={edit}>
+          <InfoUserRow title="기술스택" edit={edit} info={userDetailInfo?.techStackTags?.map((tag) => tag).join(", ")}>
             <Selector
               name="stack"
               placeholder="# 태그"
@@ -126,12 +146,8 @@ export default function Information() {
               onChange={(selected) => selectChangeHandler(selected, "stack")}
             />
           </InfoUserRow>
-          <InfoUserRow title="거주지" edit={edit}>
-            <InputField
-              placeholder="거주지"
-              defaultValue={`${state.address.address} ${state.address.zonecode}`}
-              readOnly
-            />
+          <InfoUserRow title="거주지" edit={edit} info={userDetailInfo?.address}>
+            <InputField placeholder="거주지" value={`${state.address.address} ${state.address.zonecode}`} readOnly />
             <Button
               variant="sub"
               size="small"
@@ -148,10 +164,10 @@ export default function Information() {
       </section>
       <section className={container__section}>
         <div className={container__section__content}>
-          <InfoUserRow title="이메일" isDefault />
-          <InfoUserRow title="이름" isDefault />
-          <InfoUserRow title="성별" isDefault />
-          <InfoUserRow title="생일" isDefault />
+          <InfoUserRow title="이메일" isDefault info={userBasicInfo?.email} />
+          <InfoUserRow title="이름" isDefault info={userBasicInfo?.name} />
+          <InfoUserRow title="성별" isDefault info={userBasicInfo?.gender} />
+          <InfoUserRow title="생일" isDefault info={userBasicInfo?.birthDate} />
         </div>
         <Button size="long" variant="sub" className={`${container__section__button} ${red}`}>
           게정탈퇴
