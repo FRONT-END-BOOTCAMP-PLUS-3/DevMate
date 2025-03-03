f5972e3d6d55c33168dedc02e39fcc7a7476046c
smimdla0205 <sangmin0205@gmail.com>
2025-03-03 15:09:08 +0900

    feat: /페이지 리다이렉트 추가

--- app/(anon)/page.tsx
+++ app/(anon)/page.tsx
@@ -1,8 +1,9 @@
-"use client";
-
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
+  redirect("/recruitments");
+
   return (
     <div>
       <h1>Welcome to Our Service</h1>
