3f1853d6cbdee58e9a85d3eb5f2f5a4e8f672e9d
smimdla0205 <sangmin0205@gmail.com>
2025-03-03 19:08:39 +0900

    feat: 사용자 정보 조회 기능에 주소 복호화 및 반환 타입 개선

--- application/usecases/information/getUserInfoUsecase.ts
+++ application/usecases/information/getUserInfoUsecase.ts
@@ -1,27 +1,53 @@
+import { getEncryptionKey } from "@/utils/security";
+
 import type { UserRepository } from "@/domain/repositories/userRepository";
 
+import type { InfoUserDto } from "./dtos/infoUserDto";
+
+import crypto from "crypto";
 export class GetUserInfoUsecase {
-  userRepository: UserRepository;
+  private userRepository: UserRepository;
+  private encryptionKey: Buffer;
+  private algorithm = "aes-256-cbc";
   constructor(userRepository: UserRepository) {
     this.userRepository = userRepository;
+    this.encryptionKey = getEncryptionKey();
   }
-  async execute(userId: string) {
+
+  private decryptAddress(encryptedAddress: string): string {
+    const iv = Buffer.from(encryptedAddress.slice(0, 32), "hex");
+    const encryptedData = encryptedAddress.slice(32);
+    const decipher = crypto.createDecipheriv(this.algorithm, this.encryptionKey, iv);
+
+    const decrypted = decipher.update(encryptedData, "hex", "utf8") + decipher.final("utf8"); // 🔹 const 사용
+
+    return decrypted;
+  }
+  async execute(userId: string): Promise<InfoUserDto> {
     const userData = await this.userRepository.findById(userId);
 
     const userInfo = {
-      editable: {
-        name: userData?.name,
-        email: userData?.email,
-        gender: userData?.gender,
-        birthDate: userData?.birthDate,
+      nonEditInfo: {
+        name: userData?.name || "",
+        email: userData?.email || "",
+        gender: userData?.gender
+          ? userData.gender === "MALE"
+            ? "남성"
+            : userData.gender === "FEMALE"
+              ? "여성"
+              : "기타"
+          : "",
+        birthDate: userData?.birthDate
+          ? `${new Date(userData.birthDate).getFullYear()}년 ${new Date(userData.birthDate).getMonth() + 1}월 ${new Date(userData.birthDate).getDate()}일`
+          : "생년월일이 입력되지 않았습니다.",
       },
-      nonEditable: {
-        profileImg: userData?.profileImg && "/defaultProfile.svg",
+      editInfo: {
+        profileImg: userData?.profileImg ? userData.profileImg : "/defaultProfile.svg",
         nickname: userData?.nickname,
         career: userData?.career,
         position: userData?.position,
         techStackTags: [],
-        address: userData?.address,
+        address: userData?.address ? this.decryptAddress(userData?.address) : "",
       },
     };
     return userInfo;
