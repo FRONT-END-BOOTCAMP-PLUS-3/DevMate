bbcc9d2e98097b5abd83998e7cad0362753228ad
BoYoung00 <qhdud20@naver.com>
2025-03-04 12:12:39 +0900

    rename: E2E 테스트 폴더 구조 추가

--- __tests__/e2e/(anon)/recruitments/[id]/page.test.tsx
+++ __tests__/e2e/(anon)/recruitments/[id]/page.test.tsx
@@ -3,9 +3,11 @@ import { test, expect } from "@playwright/test";
 test("SSR 페이지가 정상적으로 렌더링 되는지", async ({ page }) => {
   await page.goto("http://localhost:3000/recruitments/1"); // SSR 페이지 URL
 
-  // 모집글 제목이 정상적으로 표시되는지 확인
-  await expect(page.getByText("작성일")).toBeVisible();
+  const bodyText = await page.locator("body").innerText();
+
+  // 프로젝트 제목이 정상적으로 표시되는지 확인
+  expect(bodyText).toContain("프로젝트 제목");
 
   // 댓글이 정상적으로 표시되는지 확인
-  await expect(page.getByText("댓글")).toBeVisible();
+  expect(bodyText).toContain("댓글");
 });
