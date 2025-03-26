975184528e602d5a2db1bee6110847350427bb60
smimdla0205 <sangmin0205@gmail.com>
2025-03-26 21:25:05 +0900

    feat: 메타데이터 및 사이트 검증 정보 업데이트

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import localFont from "next/font/local";
 import { Inter } from "next/font/google";
 
@@ -9,10 +8,34 @@ import type { Metadata } from "next";
 import "@/styles/globals.scss";
 
 export const metadata: Metadata = {
-  title: "dev-mate",
-  description: "dev-mate site of the lionCat team.",
+  title: "팀 프로젝트 - devmate | 개발자 프로젝트 팀 모집 및 협업 플랫폼",
+  description:
+    "누구나 함께하는 devmate 커뮤니티. 관심있는 주제에 대한 팀프로젝트(사이드프로젝트)를 함께 해보세요. - 팀프로젝트 ㅣ devmate",
   icons: "/favicon.ico",
+  openGraph: {
+    type: "website",
+    title: "팀 프로젝트 - devmate | 개발자 프로젝트 팀 모집 및 협업 플랫폼",
+    description:
+      "누구나 함께하는 devmate 커뮤니티. 관심있는 주제에 대한 팀프로젝트(사이드프로젝트)를 함께 해보세요. - 팀프로젝트 ㅣ devmate",
+    images: ["https://dev-mate.newlecture.com/logoWidthMata.png"],
+    url: "https://dev-mate.newlecture.com",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "팀 프로젝트 - devmate | 개발자 프로젝트 팀 모집 및 협업 플랫폼",
+    description:
+      "누구나 함께하는 devmate 커뮤니티. 관심있는 주제에 대한 팀프로젝트(사이드프로젝트)를 함께 해보세요. - 팀프로젝트 ㅣ devmate",
+    images: ["https://dev-mate.newlecture.com/logoWidthMata.png"],
+  },
+  robots: "index, follow",
+  verification: {
+    google: "hQF97uWNeNaq9ByIdR4TdnK1VVwHa8r2bONGpWszwuU",
+    other: {
+      "naver-site-verification": "77749f05a3997551c0aa50899c8de1b07f3e5d54",
+    },
+  },
 };
+
 const pretendard = localFont({
   src: "../public/fonts/PretendardVariable.woff2",
   display: "swap",
@@ -28,35 +51,6 @@ const inter = Inter({
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
-      <Head>
-        <meta name="google-site-verification" content="hQF97uWNeNaq9ByIdR4TdnK1VVwHa8r2bONGpWszwuU" />
-        <meta name="naver-site-verification" content="77749f05a3997551c0aa50899c8de1b07f3e5d54" />
-        <meta
-          name="description"
-          content="누구나 함께하는 devmate 커뮤니티. 관심있는 주제에 대한 팀프로젝트(사이드프로젝트)를 함께 해보세요. - 팀프로젝트 ㅣ devmate"
-        />
-        <title>팀 프로젝트 - devmate | 개발자 프로젝트 팀 모집 및 협업 플랫폼</title>
-
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="팀 프로젝트 - devmate | 개발자 프로젝트 팀 모집 및 협업 플랫폼" />
-        <meta
-          property="og:description"
-          content="누구나 함께하는 devmate 커뮤니티. 관심있는 주제에 대한 팀프로젝트(사이드프로젝트)를 함께 해보세요. - 팀프로젝트 ㅣ devmate"
-        />
-        <meta property="og:image" content="/logoWidthMata.png" />
-        <meta property="og:url" content="https://dev-mate.newlecture.com" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="팀 프로젝트 - devmate | 개발자 프로젝트 팀 모집 및 협업 플랫폼" />
-        <meta
-          name="twitter:description"
-          content="누구나 함께하는 devmate 커뮤니티. 관심있는 주제에 대한 팀프로젝트(사이드프로젝트)를 함께 해보세요. - 팀프로젝트 ㅣ devmate"
-        />
-        <meta name="twitter:image" content="/logoWidthMata.png" />
-        <meta name="twitter:url" content="https://dev-mate.newlecture.com" />
-
-        <meta name="robots" content="index, follow" />
-      </Head>
       <body className={`${pretendard.className}${inter.className}`}>{children}</body>
     </html>
   );
